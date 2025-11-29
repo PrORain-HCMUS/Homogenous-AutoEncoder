99dfe71509e361848e8ad9471d1a233729d9873c
PrORain-HCMUS <llehoangvum10@gmail.com>
2025-11-29 13:20:16 +0700

    initial add include

---
+++ include/svm_wrapper.h
@@ -0,0 +1,7 @@
+#ifndef SVM_WRAPPER_H
+#define SVM_WRAPPER_H
+
+// Placeholder header for LIBSVM integration in later phases.
+// You can add functions here to train and evaluate SVM using extracted features.
+
+#endif  // SVM_WRAPPER_H
